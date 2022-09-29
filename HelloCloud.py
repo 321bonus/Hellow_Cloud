961988cf71c69a9ae27db09b87f663f401f53147
321bonus <s6406022620096@fitm.kmutnb.ac.th>
2022-09-29 15:20:30 +0700

    sent

---
+++ HelloCloud.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello RUK-COM!</h1>'
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    links = ['https://ruk-com.in.th',
+            'https://www.google.com',
+            'https://www.python.org',
+            'https://www.kmutnb.ac.th']
+    return render_template('example.html', links=links)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
