513227716b986d06ed37dd2c929d60357470382e
321bonus <87358955+321bonus@users.noreply.github.com>
2022-10-18 18:00:07 +0700

    Delete bonus directory

--- bonus/Bonus.sql
+++
@@ -1,7 +0,0 @@
-SELECT Students.student_id, Students.f_name, Students.l_name, Subject.subject_id, Subject.subject_name, Registration.grade, Teachers.f_name, Teachers.l_name FROM Students
-join Registration
-on Registration.student_id = Students.student_id
-JOIN Subject
-on Registration.subject_id = Subject.subject_id
-JOIN Teachers
-on Subject.teacher_id = Teachers.teacher_id
